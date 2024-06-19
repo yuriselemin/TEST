b45d91cc9b52a947c43bed97adb0fd61c1ac7de8
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-06-19 18:04:38 +0300

    Update TEST_.py

--- TEST_.py
+++ TEST_.py
@@ -24,6 +24,30 @@ print(''.join(str(x) for x in range(1, 11)))
 
 
 
+class Human:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def say_info(self):
+        print(f'Привет! меня зовут {self.name} и мне {self.age} года')
+
+    def birthday(self):
+        self.age += 1
+        print(f'Привет, у меня сегодня день рождения. Мне  {self.age} года')
+
+
+den = Human('Денис', age=22)
+max = Human('Макс', age=22)
+
+
+print(den.name, den.age)
+print(max.name, max.age)
+den.say_info()
+max.say_info()
+den.birthday()
+
+
+
 
 
 
